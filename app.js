2eebdd1bf54417312e01dd67fa2ac8b9b1e91ecc
muhammadboburshoh <muhammadbobur600@gmail.com>
2022-04-28 14:48:45 +0500

    starting monggose

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoos = require("mongoose");
 
-const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+const errorController = require("./controllers/error");
+const User = require("./models/user");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findById('626a2d43368d148c438bbf59')
-    .then(user => {
+  User.findById("626a2d43368d148c438bbf59")
+    .then((user) => {
       console.log(user, "user");
       req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 });
 
-app.use('/admin', adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000, () => console.log("3000"));
-});
+mongoos
+  .connect('mongodb+srv://muhammadbobur:1234@cluster0.a8kar.mongodb.net/test')
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
